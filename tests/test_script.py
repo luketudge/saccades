6eb09aa82bc4d07d24fd4af3d2903368ef003a49
luketudge <luketudge@gmail.com>
2019-12-04 10:20:24 +0100

    a bit more for functional test

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-This file doubles as a functional test, \
-which tests basic use of the saccades package \
-and checks only that no exceptions occur, \
-and an example script demonstrating basic use.
+This file doubles as an example script demonstrating basic use, \
+and a very minimal functional test \
+that tests basic use and checks only that no exceptions occur.
 """
 
 import os
@@ -16,9 +15,11 @@ import saccades
 
 #%% Setup
 
-BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 FILENAME = 'example.csv'
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 FILE_PATH = os.path.join(BASE_PATH, 'data', FILENAME)
+
+CENTER = (320, 240)
 
 DF = pandas.read_csv(FILE_PATH, header=None)
 
@@ -28,8 +29,14 @@ def test_script():
     ## Turn a pandas DataFrame into GazeData.
     gd = saccades.GazeData(DF)
 
+    ## Recenter.
+    gd.center(CENTER)
+
     ## Calculate velocity and acceleration.
     gd.get_accelerations()
 
     ## Display the data.
     print(gd)
+
+    ## Plot.
+    gd.plot()
