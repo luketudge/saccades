8236627c90d46242c59f3cc3e278c41b39b6d124
luketudge <luketudge@gmail.com>
2019-12-21 11:14:04 +0100

    init types for tests of gazedata done

--- tests/cases/gazedata.py
+++ tests/cases/gazedata.py
@@ -2,15 +2,11 @@
 """Test cases for gaze data.
 """
 
-import copy
-
 import numpy
 import pandas
 
 
 # %% Input data
-
-colnames = ['time', 'x', 'y']
 
 # Use a numpy array as canonical data format.
 # This makes comparisons easy with numpy.array_equal().
@@ -20,7 +16,24 @@ data = numpy.array([[0., 1., 2.],
                     [4., 5., 6.],
                     [6., 7., 8.]])
 
-data_df = pandas.DataFrame(data, columns=colnames)
+# Make some variations on pandas DataFrames,
+# since this is likely to be a common input type.
+
+colnames = ['time', 'x', 'y']
+
+data_df = pandas.DataFrame(data, columns=colnames, copy=True)
+
+indices = numpy.array(range(9000, 9000 + data.shape[0]))
+data_df_reindexed = data_df.set_index(indices)
+
+data_df_shape_only = data_df.copy()
+data_df_shape_only.columns = ['a', 'b', 'c']
+
+data_df_extra_col = data_df.copy()
+data_df_extra_col['foo'] = 'foo'
+
+data_df_reordered = data_df_extra_col.copy()
+data_df_reordered = data_df_reordered[['foo', 'y', 'time', 'x']]
 
 GAZE_DATA = {
     'list': {
@@ -42,6 +55,22 @@ GAZE_DATA = {
     'dataframe_unnamed': {
         'in':
             {'data': pandas.DataFrame(data)},
+    },
+    'dataframe_reindexed': {
+        'in':
+            {'data': data_df_reindexed},
+    },
+    'dataframe_shape_only': {
+        'in':
+            {'data': data_df_shape_only},
+    },
+    'dataframe_extra_col': {
+        'in':
+            {'data': data_df_extra_col},
+    },
+    'dataframe_reordered': {
+        'in':
+            {'data': data_df_reordered},
     },
 }
 
